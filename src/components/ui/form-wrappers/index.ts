8dcc99ba870d1b97b051f03b006937f796ce111e
spice <67798618+Downspice@users.noreply.github.com>
2026-07-07 09:02:36 +0000

    add DatePickerWrapper and FileUploadWrapper components

--- src/components/ui/form-wrappers/index.ts
+++ src/components/ui/form-wrappers/index.ts
@@ -4,3 +4,5 @@ export { default as SwitchWrapper } from './switch-wrapper';
 export { default as CheckboxWrapper } from './checkbox-wrapper';
 export { default as SliderWrapper } from './slider-wrapper';
 export { default as SelectWrapper } from './select-wrapper';
+export { default as DatePickerWrapper } from './date-picker-wrapper';
+export { default as FileUploadWrapper } from './file-upload-wrapper';
